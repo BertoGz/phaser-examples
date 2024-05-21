9dd26be244df9d2f56dafc25a3b37bbec22212e4
bertogz <bertogzlx@gmail.com>
2024-05-21 15:22:28 -0700

    change port

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use((req, res) => {
   res.status(404);
   res.send(`<h1>Error 404: Resource not found</h1>`);
 });
-
-app.listen(3000, () => {
-  console.log("App listening on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
 });
