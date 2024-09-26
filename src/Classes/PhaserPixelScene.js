8dc16df3d964d8c08f2c362c8c02e68c6094220f
bertogz <bertogzlx@gmail.com>
2024-09-25 22:12:21 -0700

    component to assist creating pixel perfect scenes

--- src/Classes/PhaserPixelScene.js
+++ src/Classes/PhaserPixelScene.js
@@ -2,21 +2,14 @@ import Phaser from "phaser";
 /**
  * @description the default pixel size relavent to the upscale factor
  */
-export let PX = 1;
-/**
- * @description lerps two values
- * @param {*} start
- * @param {*} end
- * @param {*} smoothing
- * @returns
- */
+let _upscale = 1;
 
 export function createPixelScene(config, scene) {
   const upscale = config?.upscale ?? 1;
   config.width = config.width * upscale;
   config.height = config.height * upscale;
   const game = new Phaser.Game(config);
-  PX = upscale;
+  _upscale = upscale;
   game.config.upscale = upscale;
   game.scene.add("Scene", scene);
 
